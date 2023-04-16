f3523928578db97a4044d700a93c96ef980b33bc
hamchick <chickenhamgod@gmail.com>
2023-04-16 14:26:20 +0800

    Fix some bug and add typing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,15 @@
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+
+
+<!--  -->
+<script type="text/javascript" src="typed.js"></script>
+<script type="text/javascript">
+      var typed = new Typed('.bestde', {
+    strings: ["Computer Science",'Economics', 'Blockchain', 'Chasing Dreams'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true,
+  });
+</script>
